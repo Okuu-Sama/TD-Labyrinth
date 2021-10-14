039e3c52ce204b751d461afbd418f5c702aeed68
Nicolas JATOB <nicolasjatob@orange.fr>
2021-10-15 00:11:10 +0900

    TD2 in progress

--- TD-Labyrinthe/Assets/Editor/Scripts/SceneData.cs
+++ TD-Labyrinthe/Assets/Editor/Scripts/SceneData.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class SceneData
 {
-    
+    public string sceneName;
+    public string scenePath;
+    public bool isOpen;
+    public bool displayNext;
+
+    public SceneData(string _sceneName, string _scenePath, bool _isOpen, bool _displayNext)
+    {
+        sceneName = _sceneName;
+        scenePath = _scenePath;
+        isOpen = _isOpen;
+        displayNext = _displayNext;
+    }
+
 }
--- TD-Labyrinthe/Assets/Editor/Scripts/ScenesManager.cs
+++ TD-Labyrinthe/Assets/Editor/Scripts/ScenesManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEditor.SceneManagement;
 using UnityEditor;
+using System.IO;
+using UnityEngine;
 
 public class ScenesManager : EditorWindow
 {
+    private List<SceneData> listOfScene = null;
+
     [MenuItem("Tools/Scenes Manager")]
     static void ShowWindow()
     {
@@ -14,7 +18,92 @@ public class ScenesManager : EditorWindow
 
     private void OnGUI()
     {
-        
+        GUILayout.BeginVertical();
+
+        GUILayout.BeginHorizontal("");
+        GUILayout.Label("Make sure all scenes are in the build: ");
+        if(GUILayout.Button("Check build"))
+        {
+            GetWindow(System.Type.GetType("UnityEditor.BuildPlayerWindow,UnityEditor"));
+        }
+        GUILayout.EndHorizontal();
+
+        //qqqqqqqqqqqqqqqqqqq
+
+        GUILayout.BeginHorizontal("");
+        GUILayout.Label("Select scenes to load: ");
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("Refresh list"))
+        {
+            RefreshContent();
+        }
+        GUILayout.EndHorizontal();
+
+        //GUILayout.FlexibleSpace();
+        GUILayout.EndVertical();
+
+        GUILayout.BeginVertical();
+        GUILayout.BeginScrollView(new Vector2(), GUILayout.Width(150), GUILayout.Height(250));
+
+        if(listOfScene != null)
+        {
+            foreach(var scene in listOfScene)
+            {
+                if(scene.isOpen = GUILayout.Toggle(scene.isOpen, scene.sceneName))
+                {
+                    if(scene.displayNext)
+                    {
+                        EditorSceneManager.OpenScene(scene.scenePath, OpenSceneMode.Additive);
+                        scene.displayNext = false;
+                    }
+                        
+                }
+                else
+                {
+                    if(EditorSceneManager.sceneCount == 1 && !scene.displayNext)
+                    {
+                        
+                        GetWindow<ScenesManager>().ShowNotification(new GUIContent("STOP DOING SHIT"));
+                        scene.isOpen = true;
+                    }
+                    else
+                    {
+                        EditorSceneManager.CloseScene(EditorSceneManager.GetSceneByName(scene.sceneName),true);
+                        scene.displayNext = true;
+                    }
+                }
+            }
+        }
+        GUILayout.FlexibleSpace();
+        GUILayout.EndScrollView();
+        GUILayout.FlexibleSpace();
+        GUILayout.EndVertical();
+        //GUILayout.FlexibleSpace();
+    }
+
+    void RefreshContent()
+    {
+        if(listOfScene is null)
+            listOfScene = new List<SceneData>();
+        listOfScene.Clear();
+        foreach(var scene in EditorBuildSettings.scenes)
+        {
+            listOfScene.Add(new SceneData(Path.GetFileNameWithoutExtension(scene.path),
+                                          scene.path,
+                                          false,
+                                          true));
+        }
+
+        int sceneIndex = 0;
+        for(int i = 0; i < EditorSceneManager.sceneCount;++i)
+        {
+            sceneIndex = EditorSceneManager.GetSceneAt(i).buildIndex;
+            if (sceneIndex >= 0 && sceneIndex < EditorSceneManager.sceneCount)
+            {
+                listOfScene[sceneIndex].isOpen = true;
+                listOfScene[sceneIndex].displayNext = false;
+            }
+        }
     }
 
 }
