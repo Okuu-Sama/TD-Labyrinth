8f628e57d10fc0d2490b638d00752d56c5077b31
StellaTham <stella.thammavong@gmail.com>
2021-10-19 11:35:02 +0200

    Navigation implemented

---
+++ TD-Labyrinthe/Assets/Maze/Scripts/ExtensionScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public static class ExtensionScript 
+{
+    // Start is called before the first frame update
+   /* void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+    public static float GetPathRemainingDistance(this NavMeshAgent navMeshAgent)
+    {
+        //Debug.Log("path pending is " + navMeshAgent.pathPending + ", path status is " + navMeshAgent.pathStatus + ", path corners length is " + navMeshAgent.path.corners.Length);
+        if (navMeshAgent.pathPending ||
+            navMeshAgent.pathStatus == NavMeshPathStatus.PathInvalid ||
+            navMeshAgent.path.corners.Length == 0)
+            return -1f;
+        float distance = 0.0f;
+        for (int i = 0; i < navMeshAgent.path.corners.Length-1; ++i)
+        {
+            distance += Vector3.Distance(navMeshAgent.path.corners[i], navMeshAgent.path.corners[i + 1]);
+        }
+        return distance;
+
+    }
+
+}
--- TD-Labyrinthe/Assets/Maze/Scripts/LevelManager.cs
+++ TD-Labyrinthe/Assets/Maze/Scripts/LevelManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class LevelManager : MonoBehaviour
@@ -10,9 +11,12 @@ public class LevelManager : MonoBehaviour
     private GameObject groundPrefab;
     [SerializeField]
     private GameObject wallPrefab;
+    private float maxDistance = 0f;
+    private Vector3 position;
     // Start is called before the first frame update
     void Start()
     {
+       
         int gridWidth = 10;
         int gridHeight = 10;
         int[,] grid = new int[gridWidth, gridHeight];
@@ -38,9 +42,70 @@ public class LevelManager : MonoBehaviour
             }
             text = text + "\n";
         }
-
+        int[,] gridCopy = new int[gridWidth, gridHeight];
+        for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                gridCopy[i,j] = grid[i, j];
+            }
+        }
         System.IO.File.WriteAllText("Assets/maze.txt", text);
-        instanciateMaze(grid, gridWidth, gridHeight);
+
+        instanciateMaze(gridCopy, gridWidth, gridHeight);
+        GameObject maze = GameObject.Find("Maze");
+        //Transform mazeTransform = maze.transform;
+        NavMeshSurface navMeshSurface = maze.AddComponent(typeof(NavMeshSurface)) as NavMeshSurface;
+        navMeshSurface.BuildNavMesh();
+        GameObject monkey = GameObject.Find("Monkey");
+        NavMeshAgent navMeshAgent = monkey.AddComponent(typeof(NavMeshAgent)) as NavMeshAgent;
+        //NavMeshAgent navMeshAgent = monkey.GetComponent<NavMeshAgent>();
+        bool[,] deadEnd = new bool[gridWidth, gridHeight];
+        for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                deadEnd[i, j] = false;
+            }
+        }
+
+        getDeadEnd(grid, gridWidth, gridHeight, deadEnd);
+        /*for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                Debug.Log(deadEnd[i,j]);
+            }
+        }*/
+        
+        int indexI = 0;
+        int indexJ = 0;
+        for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                if (deadEnd[i, j]==true)
+                {
+                    NavMeshPath path=new NavMeshPath();
+                    //Debug.Log("Dead end at index " + i + " " + j);
+                    
+                    navMeshAgent.CalculatePath(new Vector3(5 * i, 0, 5 * j), path);
+                    navMeshAgent.SetPath(path);
+                    float distance = ExtensionScript.GetPathRemainingDistance(navMeshAgent);
+                    //Debug.Log("Distance =  " +distance);
+                    if (distance > maxDistance)
+                    {
+                        maxDistance = distance;
+                        position = new Vector3(5 * i, 0, 5 * j);
+                        indexI = i;
+                        indexJ = j;
+                    }
+                }
+            }
+        }
+        Debug.Log("Max distance is " + maxDistance + " at index "+ indexI + " "+indexJ);
+        //navMeshAgent.Warp(new Vector3(0f,0f,0f));
+        navMeshAgent.SetDestination(position);
     }
 
     // Update is called once per frame
@@ -75,9 +140,28 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    void instanciateMaze(int[,] grid, int gridWidth, int gridHeight)
+    void getDeadEnd(int[,] grid, int gridWidth, int gridHeight, bool[,] deadEnd)
     {
 
+        for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                //Debug.Log(grid[i,j]);
+                if (grid[i, j] == 1 || grid[i, j] == 2 || grid[i, j] == 4 || grid[i, j] == 8)
+                {
+                    deadEnd[i, j] = true;
+                }
+            }
+        }
+    }
+
+    void instanciateMaze(int[,] grid, int gridWidth, int gridHeight)
+    {
+        GameObject maze = GameObject.Find("Maze");
+        Transform mazeTransform = maze.transform;
+        
+        
         bool[,] eastWall = new bool[gridWidth, gridHeight];
         for (int i = 0; i < gridWidth; i++)
         {
@@ -96,13 +180,15 @@ public class LevelManager : MonoBehaviour
             }
         }
 
+        
+
         for (int i = 0; i < gridWidth; i++)
         {
             for (int j = 0; j < gridHeight; j++)
             {
                 
                 int[] dir = new int[4] { (int)MazeGenerator.directions.W, (int)MazeGenerator.directions.E, (int)MazeGenerator.directions.S, (int)MazeGenerator.directions.N };
-                Instantiate(groundPrefab, new Vector3(5 * i, 0, 5 * j), Quaternion.identity);
+                Instantiate(groundPrefab, new Vector3(5 * i, 0, 5 * j), Quaternion.identity,mazeTransform);
                 for (int k = 0; k < dir.Length; k++)
                 {
                     if (grid[i, j] - dir[k] >= 0)
@@ -121,14 +207,14 @@ public class LevelManager : MonoBehaviour
                             {
                                 if (i == 0)
                                 {
-                                    GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity);
+                                    GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity, mazeTransform);
                                     newWall.transform.localScale = new Vector3(0.25f, 5f, 5f);
                                     newWall.transform.localPosition = new Vector3(newWall.transform.localPosition.x - 2.375f, newWall.transform.localPosition.y, newWall.transform.localPosition.z);
 
                                 }
                                 if (i > 0 && southWall[i - 1, j] == false)
                                 {
-                                    GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity);
+                                    GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity, mazeTransform);
                                     newWall.transform.localScale = new Vector3(0.25f, 5f, 5f);
                                     newWall.transform.localPosition = new Vector3(newWall.transform.localPosition.x - 2.375f, newWall.transform.localPosition.y, newWall.transform.localPosition.z);
 
@@ -137,7 +223,7 @@ public class LevelManager : MonoBehaviour
                             }
                             else
                             {
-                                GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity);
+                                GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity, mazeTransform);
                                 newWall.transform.localScale = new Vector3(0.25f, 5f, 5f);
                                 //If we put a wall at south
 
@@ -152,7 +238,7 @@ public class LevelManager : MonoBehaviour
                             {
                                 //If we put a wall at east
                                 eastWall[i, j] = true;
-                                GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity);
+                                GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity, mazeTransform);
                                 if (i == 0)
                                 {
                                     newWall.transform.localScale = new Vector3(5f, 5f, 0.25f);
@@ -174,14 +260,14 @@ public class LevelManager : MonoBehaviour
                             {
                                 if (j == 0)
                                 {
-                                    GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity);
+                                    GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity, mazeTransform);
                                     newWall.transform.localScale = new Vector3(5f, 5f, 0.25f);
                                     newWall.transform.localPosition = new Vector3(newWall.transform.localPosition.x, newWall.transform.localPosition.y, newWall.transform.localPosition.z - 2.375f);
 
                                 }
                                 if (j > 0 && eastWall[i, j - 1] == false)
                                 {
-                                    GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity);
+                                    GameObject newWall = Instantiate(wallPrefab, new Vector3(5 * i, 3, 5 * j), Quaternion.identity, mazeTransform);
                                     newWall.transform.localScale = new Vector3(5f, 5f, 0.25f);
                                     newWall.transform.localPosition = new Vector3(newWall.transform.localPosition.x, newWall.transform.localPosition.y, newWall.transform.localPosition.z - 2.375f);
 
@@ -192,8 +278,10 @@ public class LevelManager : MonoBehaviour
 
                     }
                 }
-
+                
             }
         }
     }
+
+    
 }
